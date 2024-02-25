dbefd9336cc808e76f8e9d8d238f5a348d14604a
Tianhao Wang <wth@riseup.net>
2024-02-25 04:55:09 +0100

    add mm and ds modules (yet empty)

--- src/mm/pma.rs
+++ src/mm/pma.rs
@@ -1,9 +1,31 @@
-use core::ffi::c_void;
 use crate::defs::Mem;
+use core::ffi::c_void;
+use core::{ptr, slice};
 // this is POC code, it will be ugly
+
 extern "C" {
-    static KERNEL_END: *const c_void;
+	pub fn ___KERNEL_END__();
+}
+/// Bitmap for physical frames. to get around the chicken-egg problem, we provide a provisional
+/// bitmap of fixed length in the startup code.
+pub struct PFMap {
+	bm: &'static mut [u8],
+	skip: usize, // pfn to skip (because they are already used by the initial kernel image)
+	end: usize,  // pfn limit
+}
+
+// TODO PMA initialization : needs to singleton
+
+impl PFMap {}
+
+pub struct Frame {
+	pfn: usize,
+}
+
+impl Frame {
+	pub fn addr(&self) -> usize {
+		self.pfn << Mem::PAGE_SHIFT
+	}
 }
 
 // pub struct PageAlloctor;
-
