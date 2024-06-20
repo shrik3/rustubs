3a5c0a526a761246f3dc5835ef28e3795e83caf6
Tianhao Wang <shrik3@mailbox.org>
2024-06-20 02:17:23 +0200

    proc: add simple "user" programs
    it's actually running in kernel mode, but in newly created memory
    mappings @ lower address
    
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

--- progs/raw_hello.asm
+++ progs/raw_hello.asm
@@ -1,19 +1,29 @@
-; https://jameshfisher.com/2018/03/10/linux-assembly-hello-world/
 global _start
-
 section .text
-
 _start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, world!\n",
-  mov rdx, msglen   ;   sizeof("Hello, world!\n")
-  syscall           ; );
-
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, world!", 10
-  msglen: equ $ - msg
+	mov rdi, 0xffff8000000b8640,
+	mov word [rdi], 0x4e48 ; H
+	add rdi, 0x2
+	mov word [rdi], 0x4e65 ; e
+	add rdi, 0x2
+	mov word [rdi], 0x4e6c ; l
+	add rdi, 0x2
+	mov word [rdi], 0x4e6c ; l
+	add rdi, 0x2
+	mov word [rdi], 0x4e6f ; o
+	add rdi, 0x2
+	mov word [rdi], 0x4e2c ; ,
+	add rdi, 0x2
+	mov word [rdi], 0x4e20 ;
+	add rdi, 0x2
+	mov word [rdi], 0x4e77 ; w
+	add rdi, 0x2
+	mov word [rdi], 0x4e6f ; o
+	add rdi, 0x2
+	mov word [rdi], 0x4e72 ; r
+	add rdi, 0x2
+	mov word [rdi], 0x4e6c ; l
+	add rdi, 0x2
+	mov word [rdi], 0x4e64 ; d
+	add rdi, 0x2
+	mov word [rdi], 0x4e21 ; !
