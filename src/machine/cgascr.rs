da2d74f758f0134b5911a4456f6d00a68a59731a
Tianhao Wang <shrik3@mailbox.org>
2024-06-20 21:15:38 +0200

    cgascr: improve code
    now I know better about rust, can I do better?
    
    Signed-off-by: Tianhao Wang <shrik3@mailbox.org>

--- src/machine/cgascr.rs
+++ src/machine/cgascr.rs
@@ -11,14 +11,7 @@ use core::{fmt, ptr, slice, str};
 // TODO: clean me up
 const MAX_COLS: usize = 80;
 const MAX_ROWS: usize = 25;
-const CGA_BUFFER_START: *mut u8 = P2V(0xb8000).unwrap() as *mut u8;
-const CGA_BUFFER_START_64: *mut u64 = P2V(0xb8000).unwrap() as *mut u64;
-const CGA_BUFFER_BYTE_SIZE: usize = MAX_COLS * MAX_ROWS * 2;
-
-// THESE TWO ARE USED TO DO BATCH OPERATIONS ON CGA BUFFER MEMORY, HOPEFULLY
-// MAKE IT FASTER. I.E. SETTING 4 CHARACTERS AT ONCE.
-const CGA_BUFFER_QWORD_SIZE: usize = CGA_BUFFER_BYTE_SIZE / 8;
-const CGA_BUFFER_QWORDS_PER_ROW: usize = MAX_COLS / 4;
+const CGA_BUFFER_START: usize = P2V(0xb8000).unwrap() as usize;
 
 pub struct CGAScreen {
 	pub cga_mem: &'static mut [u8],
@@ -38,7 +31,7 @@ impl CGAScreen {
 	pub fn new() -> Self {
 		let cga = Self {
 			cga_mem: unsafe {
-				slice::from_raw_parts_mut(CGA_BUFFER_START, 2 * MAX_COLS * MAX_ROWS)
+				slice::from_raw_parts_mut(CGA_BUFFER_START as *mut u8, 2 * MAX_COLS * MAX_ROWS)
 			},
 			cursor_r: 0,
 			cursor_c: 0,
@@ -57,29 +50,20 @@ impl CGAScreen {
 		self.cga_mem[index * 2 + 1] = attr;
 	}
 
-	/// print a char at the current cursor location and update the
-	/// cursor. Scroll the screen if needed.
-	pub fn putchar(&mut self, ch: char) {
+	/// print a char at the current cursor location and update the cursor.
+	/// Scroll the screen if needed. This doesn't sync the on-screen cursor
+	/// because IO takes time. the print function updates the cursor location in
+	/// the end.
+	fn putchar(&mut self, ch: char) {
 		// TODO align to next tabstop on \t ... but this will make backspace
 		// trickier ..
 		match ch {
-			'\n' => {
-				self.cursor_r += 1;
-				self.cursor_c = 0;
-				self._check_scroll();
-			}
+			'\n' => self.newline(),
 			_ => {
 				self.show(self.cursor_r, self.cursor_c, ch, self.attr);
-				self.cursor_c += 1;
-				if self.cursor_c >= MAX_COLS {
-					self.cursor_c = 0;
-					self.cursor_r += 1;
-					self._check_scroll();
-				}
+				self.advance();
 			}
 		}
-		// update the on-screen cursor.
-		self.setpos(self.cursor_r, self.cursor_c);
 	}
 
 	#[inline(always)]
@@ -93,94 +77,106 @@ impl CGAScreen {
 		} else {
 			self.cursor_c -= 1;
 		}
-		self.setpos(self.cursor_r, self.cursor_c);
+		self.sync_cursor();
 		self.show(self.cursor_r, self.cursor_c, 0 as char, self.attr);
 	}
 
-	#[inline(always)]
-	fn _check_scroll(&mut self) {
-		if self.cursor_r >= MAX_ROWS {
-			self.scroll(1);
-			self.cursor_r -= 1;
+	// this assumes 1) every line is at least qword aligned and 2) width=80chars
+	// move a whole line, overwrites the target line.
+	// this is a helper function to scroll up/down
+	fn move_line(from: usize, to: usize) {
+		if !(0..MAX_ROWS).contains(&from) {
+			return;
+		}
+		if !(0..MAX_ROWS).contains(&to) {
+			return;
+		}
+		if from == to {
+			return;
+		}
+		// every line is 80 chars, 160 bytes, 20 x 8 bytes
+		let src: usize = CGA_BUFFER_START + from * 160;
+		let dst: usize = CGA_BUFFER_START + to * 160;
+		unsafe {
+			ptr::copy_nonoverlapping(src as *mut u64, dst as *mut u64, 20);
 		}
 	}
 
-	pub fn scroll(&self, lines: u32) {
-		if lines >= MAX_ROWS as u32 {
+	// clear a line but leave the attributes
+	fn clearline(line: usize, attr: u8) {
+		let mut base: u64 = (attr as u64) << 8;
+		base += base << 16;
+		base += base << 32;
+		if !(0..MAX_ROWS).contains(&line) {
+			return;
+		}
+		unsafe {
+			slice::from_raw_parts_mut((CGA_BUFFER_START + line * 160) as *mut u64, 20).fill(base);
+		}
+	}
+
+	/// advance the cga screen's internal book keeping of the cursor location by
+	/// 1 char. Scroll up if a newline is required at the last line.
+	fn advance(&mut self) {
+		if self.cursor_c >= (MAX_COLS - 1) {
+			self.newline();
+		} else {
+			self.cursor_c += 1;
+		}
+	}
+
+	/// move cursor to the start of the next line. Scroll screen if called on
+	/// the last line.
+	fn newline(&mut self) {
+		if self.cursor_r >= (MAX_ROWS - 1) {
+			self.scroll(1);
+		} else {
+			self.cursor_r += 1;
+		}
+		self.cursor_c = 0;
+	}
+
+	pub fn scroll(&self, lines: usize) {
+		if lines >= MAX_ROWS {
 			self.clear();
 		}
 
 		if lines == 0 {
 			return;
 		}
-		// behold the magic ... (oh fuck me)
-		let mut i: usize = lines as usize - 1;
-		loop {
-			if i == MAX_ROWS {
-				break;
-			}
-			let offset_src = (i * CGA_BUFFER_QWORDS_PER_ROW) as isize;
-			let offset_dist = offset_src - (lines * CGA_BUFFER_QWORDS_PER_ROW as u32) as isize;
-			unsafe {
-				ptr::copy_nonoverlapping(
-					CGA_BUFFER_START_64.offset(offset_src),
-					CGA_BUFFER_START_64.offset(offset_dist),
-					CGA_BUFFER_QWORDS_PER_ROW,
-				);
-			}
-			i += 1;
+		// jump over first n rows since they will be overwrriten anyways;
+		for i in lines..MAX_ROWS {
+			Self::move_line(i, i - lines);
 		}
-
-		i = MAX_ROWS - lines as usize;
-		loop {
-			if i == MAX_ROWS {
-				break;
-			}
-			self.clearline(i);
-			i += 1;
+		// and clear the rest
+		for i in (MAX_ROWS - lines)..MAX_ROWS {
+			Self::clearline(i, self.attr);
 		}
-		// clear the remaining lines:
 	}
 
 	pub fn clear(&self) {
-		// remember to swap the endian..
-		let b: u8 = self.attr;
-		let mut base: u64 = (b as u64) << 8;
-		base += base << 16;
-		base += base << 32;
-
-		for i in 0..CGA_BUFFER_QWORD_SIZE {
-			unsafe { *CGA_BUFFER_START_64.offset(i as isize) = base }
+		for i in 0..MAX_ROWS {
+			Self::clearline(i, self.attr);
 		}
 	}
 
 	pub fn reset(&mut self) {
 		self.clear();
-		self.cursor_c = 0;
-		self.cursor_r = 0;
 		self.setpos(0, 0);
-	}
-
-	fn clearline(&self, line: usize) {
-		let b: u8 = self.attr;
-		let mut base: u64 = (b as u64) << 8;
-		base += base << 16;
-		base += base << 32;
-		let start_offset_qw: isize = (line as isize) * CGA_BUFFER_QWORDS_PER_ROW as isize;
-		let end_offset_qw: isize = start_offset_qw + CGA_BUFFER_QWORDS_PER_ROW as isize;
-		unsafe {
-			for i in start_offset_qw..end_offset_qw {
-				*CGA_BUFFER_START_64.offset(i) = base;
-			}
-		}
+		self.sync_cursor();
 	}
 
 	/// the on-screen cursor position is decoupled with the system's own book
 	/// keeping, i.e. we update the cursor position based on our own record, but
 	/// we never read this position back.
 	pub fn setpos(&mut self, row: usize, col: usize) {
+		self.cursor_r = row;
+		self.cursor_c = col;
+	}
+
+	fn sync_cursor(&self) {
 		// io ports for instruction register and data register
-		let offset = cal_offset(row, col);
+		let offset = cal_offset(self.cursor_r, self.cursor_c);
 		// set lower byte
 		Self::IR_PORT.outb(15_u8);
 		delay();
@@ -189,8 +185,6 @@ impl CGAScreen {
 		Self::IR_PORT.outb(14_u8);
 		delay();
 		Self::DR_PORT.outb((offset >> 8) as u8);
-		self.cursor_r = row;
-		self.cursor_c = col;
 	}
 
 	// make cursor blink (is this necessary??)
@@ -211,29 +205,18 @@ impl CGAScreen {
 		Self::DR_PORT.outb(d);
 	}
 
-	#[allow(arithmetic_overflow)]
-	pub fn getpos_offset(&self) -> u32 {
-		// read higher byte
-		Self::IR_PORT.outb(14_u8);
-		let mut offset = Self::DR_PORT.inb();
-		offset <<= 8;
-		// read lower byte
-		Self::IR_PORT.outb(15_u8);
-		offset += Self::DR_PORT.inb();
-		offset as u32
-	}
-
 	pub fn print(&mut self, s: &str) {
 		for c in s.bytes() {
 			self.putchar(c as char);
 		}
+		self.sync_cursor();
 	}
 
 	/// this is helpful for some helper text.
 	/// this will clear the bottom line
 	/// this will not update the cursor location.
 	pub fn print_at_bottom(&mut self, s: &str, attr: u8) {
-		self.clearline(24);
+		Self::clearline(24, self.attr);
 		let s = if s.len() >= MAX_COLS { &s[0..MAX_COLS] } else { s };
 		// ugly!
 		let orig_r = self.cursor_r;
